8953875df042f0435a0bef7bd1fa2bcc3310a6ca
edwardsan00 <edwardsan00@gmail.com>
2018-05-11 16:19:41 -0500

    base con gulfile

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+'use strict';
+    const   gulp = require('gulp'),
+            sass = require('gulp-sass'),
+            autoprefixer = require('gulp-autoprefixer'),
+            browserSync = require('browser-sync').create();
+
+    gulp.task('sass', () =>{
+        return gulp.src('./scss/**/**/*.scss')
+            .pipe(sass().on('Error', sass.logError))
+            .pipe(autoprefixer({
+                browsers: ['last 2 versions']
+            }))
+            .pipe(browserSync.stream())
+            .pipe(gulp.dest('./src/css'));
+    });
+
+
+    gulp.task('server', ['sass'], () =>{
+        browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+        });
+        gulp.watch('./scss/**/**/*.scss', ['sass']);
+        gulp.watch("./*.html").on('change', browserSync.reload);
+    });
+    gulp.task('default', ['server']);
